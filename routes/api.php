cde1665e073cdf38b1e770508216e4679335d067
Muhammad Taha <imtaha999@hotmail.com>
2024-07-11 17:05:54 +0500

    API endpoints created for user Signup

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// On-boarding Routes
 Route::controller(AuthController::class)->prefix('auth')->group(function () {
     Route::post('signup', 'signUp')->name('user-signup');
 });
