79207ae4d31f7896454377380e6504bcfde43676
Muhammad Taha <imtaha999@hotmail.com>
2024-07-12 11:31:31 +0500

    constants file added

---
+++ config/constants.php
@@ -0,0 +1,17 @@
+<?php
+
+
+return [
+    'user' => [
+        'unverified'    => 0,
+        'active'        => 1,
+        'blocked'       => 2,
+        'deleted'       => 3,
+    ],
+
+];
+
+
+
+
+?>
