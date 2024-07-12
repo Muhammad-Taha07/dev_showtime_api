89c03dea697fcf96b64cfe51c96a00a85febb91f
Muhammad Taha <imtaha999@hotmail.com>
2024-07-12 15:37:33 +0500

    Added protected method for generating OTP on the basis of provided digit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,10 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    protected function generateOtp($digits)
+    {
+        $otp_code = rand(pow(10, $digits - 1), pow(10, $digits) - 1);
+        return $otp_code;
+    }
 }
