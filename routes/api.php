b381b65d2d42d5f8e82fe815aa647e98925fa829
Muhammad Taha <imtaha999@hotmail.com>
2024-07-12 15:37:47 +0500

    further routes added.

--- routes/api.php
+++ routes/api.php
@@ -22,8 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 // On-boarding Routes
 Route::controller(AuthController::class)->prefix('auth')->group(function () {
     Route::post('/signup', 'signUp')->name('user-signup');
-
+    Route::post('/send-code', 'sendCode')->name('user-send-code');
     Route::post('/verifyCode', 'verifyCode')->name('user-verify-account');
-
     Route::post('/resetPassword', 'resetPassword')->name('user-resetPass');
 });
