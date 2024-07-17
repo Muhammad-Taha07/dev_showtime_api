146e5b074cf8930ad49c28bdbe1ddc038fa5eb4a
Muhammad Taha <imtaha999@hotmail.com>
2024-07-18 01:46:03 +0500

    Relationship implemented for User model

---
+++ app/Models/UserDetails.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+class UserDetails extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 
+        'gender', 
+        'image', 
+        'address',
+        'user_age',
+        'longitude',
+        'latitude',
+        'bio',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function users()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
