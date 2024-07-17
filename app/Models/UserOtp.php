eb57acb7ad568ef751377b6162fd554320773f64
Muhammad Taha <imtaha999@hotmail.com>
2024-07-18 01:46:30 +0500

    relationship added with User Model

--- app/Models/UserOtp.php
+++ app/Models/UserOtp.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,4 +13,9 @@ class UserOtp extends Model
     protected $fillable = [
         'code', 'user_id', 'is_expired','type'
     ];
+
+    public function users()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
