b4925cc0bd4ab7099e604c68cd55f80805fff5ff
Muhammad Taha <imtaha999@hotmail.com>
2024-07-20 01:09:02 +0500

    Video Creation implemented using Spatie package

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use App\Models\Video;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Responses\BaseResponse;
+use Illuminate\Support\Facades\Auth;
 
 class VideoController extends Controller
 {
@@ -15,21 +19,36 @@ class VideoController extends Controller
 
     public function saveVideo(Request $request)
     {
-        $data = [
-            'user_id'=> $this->currentUser->id,
-            'description'=>$request->description,
-            'title'=>$request->title,
-        ];
+        try {
+            DB::beginTransaction();
 
-        Video::create($data)
-        ->addMedia($request->file('video'))
-        ->toMediaCollection();
-        dd("done");
+            $data = [
+                'user_id'       =>  $this->currentUser->id,
+                'title'         =>  $request->title,
+                'description'   =>  $request->description,
+            ];
+            
+            // Creating the video record
+            $video = Video::create($data);
+            $mediaItem = $video->addMedia($request->file('video'))->toMediaCollection();
 
-        // $video = Video::where('id', 1)->first();
-        // $mediaItems = $video->getMedia();
-        // dd($mediaItems->first()->getUrl());
-        // $videoUrl = $mediaItems->first()->getUrl();
+            DB::commit();
+
+            // Creating Response data for API
+            $response = [
+                'user_id'       => $video->user_id,
+                'title'         => $video->title,
+                'description'   => $video->description,
+                'video_url'     => $mediaItem->getUrl(),
+            ];
+
+            return new BaseResponse(STATUS_CODE_OK, STATUS_CODE_OK, "Video Uploaded Successfully", $response);
+            
+        } catch (Exception $e) {
+            DB::rollback();
+            return new BaseResponse(STATUS_CODE_BADREQUEST, STATUS_CODE_BADREQUEST, $e->getMessage() . $e->getLine() . $e->getFile() . $e);
+        }
+
     }
     
 }
