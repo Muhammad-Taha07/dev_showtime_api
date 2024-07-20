2dd6ef9c422c66e49a4808cd98a19ac9e65627c6
Muhammad Taha <imtaha999@hotmail.com>
2024-07-20 22:15:16 +0500

    Validation Request file for Video Creation

--- app/Http/Requests/MediaRequest/CreateVideoRequest.php
+++ app/Http/Requests/MediaRequest/CreateVideoRequest.php
@@ -11,18 +11,53 @@ class CreateVideoRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
+/**
+* Get the validation rules that apply to the request.
+*
+* @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+*/
     public function rules(): array
     {
         return [
-            //
+            'title'         => 'required|min:3',
+            'description'   => 'required',
+            'video'         => 'required|file|mimetypes:video/mp4|max:51200'
+        ];
+    }
+
+/**
+* Get the error messages for the defined validation rules.
+*
+* @return array
+*/
+
+    
+    public function messages(): array
+    {
+        return [
+            '*.required'        => ':attribute is required',
+            'title.min'         => ':attribute should be minimum of :min characters',
+            'video.file'        => 'The :attribute must be a file.',
+            'video.mimetypes'   => 'The :attribute must be a video of type: :values.',
+            'video.max'         => 'The :attribute may not be greater than 50 MB in size.',
+        ];
+    }
+
+/**
+ * Get custom attributes for validator errors.
+ *
+ * @return array
+ */
+
+    public function attributes(): array
+    {
+        return [
+            'title'       => 'Title',
+            'description' => 'Description',
+            'video'       => 'Video',
         ];
     }
 }
