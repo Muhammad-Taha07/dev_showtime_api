eabad8919e934d85c363c8cf839806a374e0acbb
Muhammad Taha <imtaha999@hotmail.com>
2024-07-21 01:53:38 +0500

    Added softdelete on Video model

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use Spatie\MediaLibrary\HasMedia;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Video extends Model implements HasMedia
 {
-    use HasFactory, InteractsWithMedia;
+    use HasFactory, SoftDeletes, InteractsWithMedia;
 
     protected $fillable = [
         'user_id',
