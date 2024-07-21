0b3c115aff66f1748157675c3173cc619d9cdc41
Muhammad Taha <imtaha999@hotmail.com>
2024-07-21 23:05:26 +0500

    removed unnessary commented code

--- database/migrations/2024_07_14_123703_create_user_otps_table.php
+++ database/migrations/2024_07_14_123703_create_user_otps_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('user_otps', function (Blueprint $table) {
             $table->id();
-            // $table->string('code', 64);
             $table->unsignedBigInteger('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->unsignedInteger('code',false)->nullable();
             $table->integer('otp_attempts')->default(0)->nullable();
