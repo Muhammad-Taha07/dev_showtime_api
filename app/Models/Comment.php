96371e6aa5e8c8584984efb0cd6ec48867509cfe
Muhammad Taha <imtaha999@hotmail.com>
2024-07-23 11:53:16 +0500

    Relationship between videos & users and comments added in Comment model

---
+++ app/Models/Comment.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Comment extends Model
+{
+    use HasFactory, softDeletes;
+
+    protected $fillable = ['user_id', 'video_id', 'comment', 'created_at', 'updated_at'];
+
+    public function user() 
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function video()
+    {
+        return $this->belongsTo(Video::class, 'video_id');
+    }
+}
