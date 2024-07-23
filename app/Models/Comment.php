ba099ea2dcac7f6552ee291c6899ceba37cb608f
Muhammad Taha <imtaha999@hotmail.com>
2024-07-23 16:23:04 +0500

    Relationship added for reporting Comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\User;
+use App\Models\ReportedComment;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -21,5 +22,10 @@ class Comment extends Model
     public function video()
     {
         return $this->belongsTo(Video::class, 'video_id');
+    }
+
+    public function reportedComments()
+    {
+        return $this->hasMany(ReportedComment::class);
     }
 }
