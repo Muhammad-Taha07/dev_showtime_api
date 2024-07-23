d1a05688452758736b8fbfb702adb278408ed39a
Muhammad Taha <imtaha999@hotmail.com>
2024-07-23 16:24:38 +0500

    Routes added for Reporting comments

--- routes/api.php
+++ routes/api.php
@@ -54,6 +54,7 @@ Route::middleware("auth:api")->prefix('user')->group(function () {
         Route::get('/get-comments/{id}', 'getComments')->name('get-comments');
         Route::post('/post-comment', 'postComment')->name('post-comment');
         Route::delete('/delete-comment/{id}', 'deleteComment')->name('user-delete-comment');
+        Route::post('/report-comment', 'reportComment')->name('report-comment');
     });
 });
 
