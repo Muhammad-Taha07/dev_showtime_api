5c7ac74150602e06cad7aefe1bc679d26a091561
Muhammad Taha <imtaha999@hotmail.com>
2024-07-24 12:15:38 +0500

    Relationship added for User & Media Collection in MediaLike model

--- app/Models/MediaLike.php
+++ app/Models/MediaLike.php
@@ -2,12 +2,28 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use App\Models\MediaCollection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class MediaLike extends Model
 {
     use HasFactory;
     
     protected $fillable = ['user_id', 'media_collection_id', 'rating', 'created_at', 'updated_at'];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function mediaCollection()
+    {
+        return $this->belongsTo(MediaCollection::class);
+    }
+
+
+
 }
