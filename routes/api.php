977848abe82d7c2641b3bec0fbe83f3d9d06b2bf
Muhammad Taha <imtaha999@hotmail.com>
2024-07-26 00:17:19 +0500

    middleware added into routes & route for banning user accounts

--- routes/api.php
+++ routes/api.php
@@ -29,7 +29,7 @@ Route::controller(AuthController::class)->prefix('auth')->group(function () {
 });
 
 // Authenticated Routes
-Route::middleware("auth:api")->prefix('user')->group(function () {
+Route::middleware(["auth:api", "check.user.status"])->prefix('user')->group(function () {
     Route::controller(AuthController::class)->group(function () {
         Route::post('/logout', 'logout')->name('logout');
         Route::post('/reset-password', 'resetPassword')->name('user-reset-password');
@@ -67,6 +67,7 @@ Route::middleware(['auth:api', 'check.admin'])->prefix('admin')->group(function 
         Route::get('/get-admin-mediafiles', 'getMediaFiles')->name('admin-get-pending-media');
         Route::post('/update-media-status', 'updateMediaStatus')->name('admin-approve-media');
         Route::get('/get-reported-comments', 'getReportedComments')->name('admin-get-reports');
+        Route::post('/ban-user/{id}', 'banUserAccount')->name('admin-ban-user');
     });
 });
 
