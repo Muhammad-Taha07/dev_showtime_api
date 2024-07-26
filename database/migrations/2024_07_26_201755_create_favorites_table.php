b134408bc024882af7c89e0bcec0511b58717f2b
Muhammad Taha <imtaha999@hotmail.com>
2024-07-27 01:57:37 +0500

    migration file for adding/removing from favorites

---
+++ database/migrations/2024_07_26_201755_create_favorites_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('media_collection_id');
+            $table->foreign('media_collection_id')->references('id')->on('media_collections')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('favorites');
+    }
+};
