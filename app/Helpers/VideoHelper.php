683d9b4b5550100fc7a94cc2ae2dc953d6bfed0f
Muhammad Taha <imtaha999@hotmail.com>
2024-08-06 23:56:54 +0500

    removed commented line of codes

--- app/Helpers/VideoHelper.php
+++ app/Helpers/VideoHelper.php
@@ -8,35 +8,16 @@ use Illuminate\Support\Facades\Storage;
 if(!function_exists('generateThumbnail')) {
     function generateThumbnail($videoPath)
     {
-
         $ffmpeg = FFMpeg::create([
-            'ffmpeg.binaries'  => base_path('storage/ffmpeg/ffmpeg/bin/ffmpeg.exe'),
-            'ffprobe.binaries' => base_path('storage/ffmpeg/ffmpeg/bin/ffprobe.exe'),
+            'ffmpeg.binaries'  => base_path(env('FFMPEG_BINARIES_PATH')),
+            'ffprobe.binaries' => base_path(env('FFPROBE_BINARIES_PATH')),
         ]);
 
         $video = $ffmpeg->open($videoPath);
-        $frame = $video->frame(TimeCode::fromSeconds(1));
+        $frame = $video->frame(TimeCode::fromSeconds(10));
 
-        // Construct the thumbnail path in the same directory as the video
-        // $thumbnailPath = dirname($videoPath) . '/thumb_' . basename($videoPath, '.' . pathinfo($videoPath, PATHINFO_EXTENSION)) . '.png';
-        // $frame->save($thumbnailPath);
-
-        // $thumbnailPath = dirname($videoPath) . '/thumb_' . basename($videoPath, '.' . $file->getClientOriginalExtension()) . '.png';
-        //   $thumbnailPath = dirname($videoPath) . '/thumb_' . basename($videoPath, '.' . pathinfo($videoPath, PATHINFO_EXTENSION)) . '.png';
-          $thumbnailPath = dirname($videoPath) . '/thumb_' . pathinfo($videoPath, PATHINFO_FILENAME) . '.png';
-
-          $frame->save($thumbnailPath);
-
-          // Resize and crop the thumbnail
-        //   $ffmpeg->open($thumbnailPath)
-        
-        //     ->save(new \FFMpeg\Format\Video\X264(), $thumbnailPath);
-
-
-        // $thumbnail->save($thumbnailPath);
-    
-        // $thumbnailUrl = url('medias/' . $media_id . '/thumb_' . basename($videoPath, '.' . $file->getClientOriginalExtension()) . '.png');
-
+        $thumbnailPath = dirname($videoPath) . '/thumb_' . pathinfo($videoPath, PATHINFO_FILENAME) . '.png';
+        $frame->save($thumbnailPath);
 
         return $thumbnailPath;
     }
