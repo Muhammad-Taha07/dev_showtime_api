dd903764696241fc0ef898cfa0a3fb9e7d01b178
Muhammad Taha <imtaha999@hotmail.com>
2024-08-16 11:15:30 +0500

    Added parent function in base controller for inheritance

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,27 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+
+    // public function pushNotification($currentUser, $otherUser, $message, $notificationType = '') {
+
+    //     $extras = [
+    //         'notification_type' => $notificationType,
+    //         'message' =>  $message,
+    //         'sender_id' => $currentUser->id,
+    //         'notify_user_type' => $currentUser->is_admin == 1 ? 'admin' : 'user',
+    //         'other_user_type' => $otherUser->is_admin == 1 ? 'admin' : 'user',
+    //     ];
+
+    //     $tokens[$otherUser->id] = $otherUser->fcm_token;
+    //     if ($otherUser->fcm_token) {
+    //         sendPushNotification(
+    //             'Showtime',
+    //             $message,
+    //             $tokens,
+    //             $extras,
+    //             true
+    //         );
+    //     }
+    // }
 }
