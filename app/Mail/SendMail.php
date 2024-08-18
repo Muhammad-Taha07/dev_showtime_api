4b2d088f1414e55e8b55b84ddf45e60217579981
Muhammad Taha <imtaha999@hotmail.com>
2024-08-18 16:27:04 +0500

    implemented mailer in laravel for sending emails

---
+++ app/Mail/SendMail.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SendMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $details;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($details)
+    {
+        $this->details = $details;
+    }
+
+    public function build()
+    {
+        return $this->subject('Showtime Mailing')
+	        ->view('email_views.register_email');
+    }
+}
