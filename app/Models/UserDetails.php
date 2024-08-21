2c978e2999fd0dd8603b9803e695d6bc3a72d829
Muhammad Taha <imtaha999@hotmail.com>
2024-08-21 22:54:38 +0500

    getter implemented to return image with URL

--- app/Models/UserDetails.php
+++ app/Models/UserDetails.php
@@ -27,4 +27,8 @@ class UserDetails extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function getImageAttribute($value) {
+        return url($value);
+    }
 }
