46f09fa1b37541989f95a6e2a5e18ed4a5ff558e
Muhammad Taha <imtaha999@hotmail.com>
2024-09-12 14:34:42 +0500

    -

--- resources/views/email_views/register_email.blade.php
+++ resources/views/email_views/register_email.blade.php
@@ -114,7 +114,7 @@ table, td { color: #000000; } #u_body a { color: #0000ee; text-decoration: under
 <table width="100%" cellpadding="0" cellspacing="0" border="0">
   <tr>
     <td style="padding-right: 0px;padding-left: 0px;" align="center">
-      <img align="center" border="0" src="{{ asset('resource_images/email_images/image-5.png') }}"   alt="image" title="image" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: inline-block !important;border: none;height: auto;float: none;width: 100%;max-width: 190px;" width="190"/>
+      <img align="center" border="0" src="http://16.170.242.172/dev_showtime_api/public/resource_images/email_images/image-5.png"   alt="image" title="image" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: inline-block !important;border: none;height: auto;float: none;width: 100%;max-width: 190px;" width="190"/>
     </td>
   </tr>
 </table>
@@ -222,7 +222,7 @@ table, td { color: #000000; } #u_body a { color: #0000ee; text-decoration: under
             <table align="center" border="0" cellspacing="0" cellpadding="0" width="32" height="32" style="width: 32px !important;height: 32px !important;display: inline-block;border-collapse: collapse;table-layout: fixed;border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;margin-right: 15px">
               <tbody><tr style="vertical-align: top"><td align="center" valign="middle" style="word-break: break-word;border-collapse: collapse !important;vertical-align: top">
                 <!-- <a href="#" title="Facebook" target="_blank"> -->
-                  <img src="{{ asset('resource_images/email_images/image-1.png') }}" alt="Facebook" title="Facebook" width="32" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: block !important;border: none;height: auto;float: none;max-width: 32px !important">
+                  <img src="http://16.170.242.172/dev_showtime_api/public/resource_images/email_images/image-1.png" alt="Facebook" title="Facebook" width="32" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: block !important;border: none;height: auto;float: none;max-width: 32px !important">
                 <!-- </a> -->
               </td></tr>
             </tbody></table>
@@ -230,7 +230,7 @@ table, td { color: #000000; } #u_body a { color: #0000ee; text-decoration: under
             <table align="center" border="0" cellspacing="0" cellpadding="0" width="32" height="32" style="width: 32px !important;height: 32px !important;display: inline-block;border-collapse: collapse;table-layout: fixed;border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;margin-right: 15px">
               <tbody><tr style="vertical-align: top"><td align="center" valign="middle" style="word-break: break-word;border-collapse: collapse !important;vertical-align: top">
                 <!-- <a href="#" title="LinkedIn" target="_blank"> -->
-                  <img src="{{ asset('resource_images/email_images/image-3.png') }}" alt="LinkedIn" title="LinkedIn" width="32" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: block !important;border: none;height: auto;float: none;max-width: 32px !important">
+                  <img src="http://16.170.242.172/dev_showtime_api/public/resource_images/email_images/image-3.png" alt="LinkedIn" title="LinkedIn" width="32" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: block !important;border: none;height: auto;float: none;max-width: 32px !important">
                 <!-- </a> -->
               </td></tr>
             </tbody></table>
@@ -238,7 +238,7 @@ table, td { color: #000000; } #u_body a { color: #0000ee; text-decoration: under
             <table align="center" border="0" cellspacing="0" cellpadding="0" width="32" height="32" style="width: 32px !important;height: 32px !important;display: inline-block;border-collapse: collapse;table-layout: fixed;border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;margin-right: 15px">
               <tbody><tr style="vertical-align: top"><td align="center" valign="middle" style="word-break: break-word;border-collapse: collapse !important;vertical-align: top">
                 <!-- <a href="#" title="Instagram" target="_blank"> -->
-                  <img src="{{ asset('resource_images/email_images/image-2.png') }}" alt="Instagram" title="Instagram" width="32" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: block !important;border: none;height: auto;float: none;max-width: 32px !important">
+                  <img src="http://16.170.242.172/dev_showtime_api/public/resource_images/email_images/image-2.png" alt="Instagram" title="Instagram" width="32" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: block !important;border: none;height: auto;float: none;max-width: 32px !important">
                 <!-- </a> -->
               </td></tr>
             </tbody></table>
@@ -246,7 +246,7 @@ table, td { color: #000000; } #u_body a { color: #0000ee; text-decoration: under
             <table align="center" border="0" cellspacing="0" cellpadding="0" width="32" height="32" style="width: 32px !important;height: 32px !important;display: inline-block;border-collapse: collapse;table-layout: fixed;border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;margin-right: 0px">
               <tbody><tr style="vertical-align: top"><td align="center" valign="middle" style="word-break: break-word;border-collapse: collapse !important;vertical-align: top">
                 <!-- <a href="" title="X" target="_blank"> -->
-                  <img src="{{ asset('resource_images/email_images/image-4.png') }}" alt="X" title="X" width="32" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: block !important;border: none;height: auto;float: none;max-width: 32px !important">
+                  <img src="http://16.170.242.172/dev_showtime_api/public/resource_images/email_images/image-4.png" alt="X" title="X" width="32" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: block !important;border: none;height: auto;float: none;max-width: 32px !important">
                 <!-- </a> -->
               </td></tr>
             </tbody></table>
